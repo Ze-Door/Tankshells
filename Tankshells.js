d5ff514c616be9c36a5d0c86bb709cb0e9e722e8
Ze-Door <216771935+Ze-Door@users.noreply.github.com>
2026-09-11 15:59:51 +0200

    Add files via upload

--- Tankshells.js
+++ Tankshells.js
@@ -136,21 +136,21 @@ var tick = (elapsedTime, multiplier) => {
                                     (Math.PI *
                                     getP(P.level) *
                                     getL(L.level+1) *
-                                    Math.pow((getd(d.level+1))*15, 2)
+                                    Math.pow((d.level+1))*15, 2)
                                     )
                                     /(2 * getm(m.level) + 1)
-                                    );
+                                    ;
     }
     if (FunctionLVL.level == 1) {
     currency.value += dt * bonus * (getm(m.level) * (
                                         (Math.PI *
                                         getP(P.level) *
                                         getL(L.level+1) *
-                                        Math.pow((getd(d.level+1))*15, 2)
+                                        Math.pow((d.level+1))*15, 2)
                                         )
                                         /(2 * getm(m.level) + 1)
                                         )
-                                        )/2
+                                        /2
                                         }
     }
 

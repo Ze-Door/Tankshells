7d27e1eee5aefc8e0be039c5057105fb902d7b0b
Ze-Door <216771935+Ze-Door@users.noreply.github.com>
2026-09-11 15:47:10 +0200

    Add files via upload

--- Tankshells.js
+++ Tankshells.js
@@ -17,12 +17,11 @@ var id = "Tankshells";
 var name = "Tankshells";
 var description = "Tankshells";
 var authors = "Door";
-var version = 1;
+var version = "0.0.2" ;
 
 var currency;
-var c1, c2, P;
-var d, L, m;
-var c1Exp, c2Exp;
+var d, L, m, P;
+var s, theta, E;
 var FunctionLVL;
 
 var achievement1, achievement2;
@@ -35,7 +34,7 @@ var init = () => {
     // Regular Upgrades
 
 
-    // C1
+    // m
     {
         let getDesc = (level) => "m=" + getm(level).toString(0) + "mg";
         m = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(2, Math.log2(1.3))));
@@ -47,45 +46,59 @@ var init = () => {
 
     // P
     {
-        let getDesc = (level) => "P=" + getP(level).toString(2) + "mpa";
-        P = theory.createUpgrade(2, currency, new FirstFreeCost(new ExponentialCost(1, Math.log2(2))));
+        let getDesc = (level) => "P=" + getP(level).toString(1) + "mpa";
+        P = theory.createUpgrade(1, currency, new FirstFreeCost(new ExponentialCost(1, Math.log2(2))));
         P.getDescription = (_) => Utils.getMath(getDesc(P.level));
         P.getInfo = (amount) => Utils.getMathTo(getDesc(P.level), getDesc(P.level + amount));
     }
 
     // L
     {
-        let getDesc = (level) => "L=" + getL(level).toString(3) + "mm";
-        L = theory.createUpgrade(3, currency, new ExponentialCost(10, Math.log2(1.3)));
+        let getDesc = (level) => "L=" + getL(level).toString(2) + "mm";
+        L = theory.createUpgrade(2, currency, new ExponentialCost(10, Math.log2(1.3)));
         L.getDescription = (_) => Utils.getMath(getDesc(L.level));
         L.getInfo = (amount) => Utils.getMathTo(getDesc(L.level), getDesc(L.level + amount));
     }
 
+    /* s
+         {
+        if (FunctionLVL.level >= 1) {
+            let getDesc = (level) => "s=" + getL(level).toString(3) + "mm";
+            s = theory.createUpgrade(3, currency, new ExponentialCost(10, 2));
+           s.getDescription = (_) => Utils.getMath(getDesc(s.level));
+           s.getInfo = (amount) => Utils.getMathTo(getDesc(s.level), getDesc(s.level + amount));
+        }
+    */ 
+
+
+    
 
 
     /////////////////////
     // Permanent Upgrades
     theory.createPublicationUpgrade(0, currency, 1e4);
-    theory.createBuyAllUpgrade(1, currency, 1e7);
-    theory.createAutoBuyerUpgrade(2, currency, 1e10);
+    theory.createBuyAllUpgrade(1, currency, 1e6);
+    theory.createAutoBuyerUpgrade(2, currency, 1e8);
 
     ///////////////////////
     //// Milestone Upgrades
 
     theory.setMilestoneCost(new LinearCost(0, 1));
-    {
-    d = theory.createMilestoneUpgrade(0, 5);
-    d.description = "Increase the Diameter of the barrel by 15cm";
-    d.boughtOrRefunded = (_) => 
-    {
-    d.info = (d.level * 15 + 15) + "cm to " + ((d.level + 1) * 15 + 15) + "cm";
-    theory.invalidatePrimaryEquation();
-    };}
+
+    // d
+        {
+        d = theory.createMilestoneUpgrade(0, 5);
+        d.description = "Increase the Diameter of the barrel by 15cm";
+        d.boughtOrRefunded = (_) => 
+        {
+        d.info = (d.level * 15 + 15) + "cm to " + ((d.level + 1) * 15 + 15) + "cm";
+        theory.invalidatePrimaryEquation();
+        };}
 
 
     // Function
     
-        FunctionLVL = theory.createMilestoneUpgrade(2, 1);
+        FunctionLVL = theory.createMilestoneUpgrade(1, 1);
         FunctionLVL.description = "Calculate the impact force of the shell";
         FunctionLVL.boughtOrRefunded = (_) => 
         {
@@ -105,37 +118,54 @@ var init = () => {
     ///////////////////
     //// Story chapters
     chapter1 = theory.createStoryChapter(0, "The diameter...", "So you decided to increase something\n\nThe diameter of course\nbigger round equals bigger result, no?\n\nThis HOPEFULLY will speed up the progress...\nor?", () => d.level > 0);
-    chapter2 = theory.createStoryChapter(1, "You cant continue like this...", "Your calculations are slowing down...\nTo a point where you can't continue...\n\nA function change is needed.\nNow\n\nBut what will you do?\n\n\n\nImpact...\nImpact force of the shell...\nYes.\nYES!\nThis is it.\nThis will be the breaktrough.", () => currency.value > 1e35);
+    chapter2 = theory.createStoryChapter(1, "You cant continue like this...", "Your calculations are slowing down...\nTo a point where you can't continue...\n\nA function change is needed.\nNow\n\nBut what will you do?\n\n\n\nImpact...\nImpact force of the shell...\nYes.\nYES!\nThis is it.\nThis will be the breaktrough.", () => theory.tau > 31.622776601683793);
 
     updateAvailability();
-}
+    }
 
 var updateAvailability = () => {
-    FunctionLVL.isAvailable = (currency.value > 1e40);
+    FunctionLVL.isAvailable = (theory.tau > 31.622776601683793);
 }
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
-    currency.value += 10 * dt * bonus *  getm(m.level) *
+    if (FunctionLVL.level == 0) {
+    currency.value += dt * bonus *  getm(m.level) *
                                     Math.sqrt(
                                     (Math.PI *
                                     getP(P.level) *
                                     getL(L.level+1) *
-                                    Math.pow((d.level+1)*15, 2)
+                                    Math.pow((getd(d.level+1))*15, 2)
                                     )
                                     /(2 * getm(m.level) + 1)
                                     );
-}
+    }
+    if (FunctionLVL.level == 1) {
+    currency.value += dt * bonus * (getm(m.level) * (
+                                        (Math.PI *
+                                        getP(P.level) *
+                                        getL(L.level+1) *
+                                        Math.pow((getd(d.level+1))*15, 2)
+                                        )
+                                        /(2 * getm(m.level) + 1)
+                                        )
+                                        )/2
+                                        }
+    }
 
-var getPrimaryEquation = () => "\\dot{\\rho}" + "=vm";
-                               
+
+var getPrimaryEquation = () => {if (FunctionLVL.level == 0) {
+                                    return "\\dot\\rho" + " = " + "vm";}
+                                if (FunctionLVL.level == 1) {
+                                    return "\\dot\\rho" + " = " + "E" + "=\\frac{1}{2}v^2" + "m";}
+                                }
 
 var getSecondaryEquation = () => {
 
     theory.secondaryEquationScale = 1.5;
     theory.secondaryEquationHeight = 100;
-    let result ="v = \\sqrt\\frac{P \\pi d^2 \\L}{2m}";
+    let result ="v = \\sqrt\\frac{P \\pi d^2 L}{2m}";
     return result;
 
 };
